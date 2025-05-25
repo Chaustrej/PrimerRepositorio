c4f19d27294eed6fcf73f5e2ac424142d3c167e0
Chaustrej <131916274+Chaustrej@users.noreply.github.com>
2025-05-25 17:10:30 -0400

    a
    Inicio

---
+++ Hola.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;    
+int main() {
+    cout << "Hola, mundo!" << endl; // Output a greeting message
+    cout << "Press any key to exit..." << endl; // Prompt user to press a key
+    _getch(); // Wait for a key press
+    return 0; // Exit the program
+}
