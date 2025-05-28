7adaee0016c71a221079cca952913a0a790869c7
Chaustrej <131916274+Chaustrej@users.noreply.github.com>
2025-05-28 11:36:24 -0400

    Un poquito de avance

--- Taller 1/Programa.cpp
+++ Taller 1/Programa.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <conio.h>
+#include <windows.h> // funcion que sirve para las funciones de la consola
+#include <string> 
+#include <fstream>
 
 using namespace std;
 
+// Definire las funciones que usare en el programa
+void CambiarColor(int color) { 
+    // Cambia el color del texto en la consola, usare esto para resaltar las letras de busquedad 
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
 int main() { 
 
 
-    
+
 }
