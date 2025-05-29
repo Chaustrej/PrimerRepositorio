da99263348846f1fea2cc63ee7caf1cbb97e89b2
Chaustrej <131916274+Chaustrej@users.noreply.github.com>
2025-05-29 19:41:45 -0400

    Adelanto

--- Taller 1/Programa.cpp
+++ Taller 1/Programa.cpp
@@ -15,6 +15,45 @@ int contador = 0; // Contador de palabras encontradas
 int main() { 
 string LasPalabras, Laslineas, continuar;
 
+    string palabra, linea, continuar;
+    
+    cout << "*** BUSCADOR ***\n";
+    cin.ignore();
+    
+    do {
+        ifstream archivo("Archivo.txt");
+        if (!archivo) {
+            cout << "Error: archivo 'Archivo.txt' no encontrado\n";
+            return 1;
+        }
+        
+        do {
+            cout << "\nPalabra a buscar: ";
+            getline(cin, palabra);
+        } while (palabra.empty());
+        
+        cout << "\n--- Resultados ---\n";
+        
+        int total = 0, Laslineas_num = 1;
+        bool encontrado = false;
+        
+        while (getline(archivo, Laslineas)) {
+            int Eltiempo = 0;
+            string LineaMarcadaColor = Marcarr(Laslineas, LasPalabras, Eltiempo);
+            
+            if (Eltiempo> 0) {
+                cout << Laslineas_num << ": " << LineaMarcadaColor << endl;
+                total += Eltiempo;
+                encontrado = true;
+            }
+           Laslineas_num++;
+        }
+        
+        archivo.close();
+        
+
+
+
 
 }
 
@@ -26,11 +65,11 @@ string Minuscula(string palabra) {
 
 string Marcarr(const string& LasPalabras, const string& Laslineas, int& Eltiempo) {
         string resultado = "";
-    string lineaMin = Minuscula(Laslineas);
-    string palabraMin = Minuscula(LasPalabras);
+    string lineaMinima = Minuscula(Laslineas);
+    string palabraMinima = Minuscula(LasPalabras);
     size_t pos = 0, Encontro;
     
-    while ((Encontro = lineaMin.find(palabraMin, pos)) != string::npos) {
+    while ((Encontro = lineaMinima.find(palabraMinima, pos)) != string::npos) {
         resultado += Laslineas.substr(pos, Encontro - pos);
         resultado += "\033[31m" + Laslineas.substr(Encontro, LasPalabras.length()) + "\033[0m";
         contador++;
@@ -39,4 +78,4 @@ string Marcarr(const string& LasPalabras, const string& Laslineas, int& Eltiempo
     resultado += Laslineas.substr(pos);
     return resultado;
 
-}
+}   
