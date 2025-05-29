23936d7912081374e2c347d3304b54914a2bdfff
Chaustrej <131916274+Chaustrej@users.noreply.github.com>
2025-05-29 19:29:03 -0400

    Creando los string

--- Taller 1/Programa.cpp
+++ Taller 1/Programa.cpp
@@ -1,17 +1,29 @@
 #include <iostream>
 #include <conio.h>
-#include <windows.h> // funcion que sirve para las funciones de la consola
 #include <string> 
 #include <fstream>
+#include <algorithm>
 
+void ();
 using namespace std;
-
+// prototipo de funciones
+string Minuscula();
+string Marcarr(const string& LasPalabras, const string& Laslineas, int& Eltiempo);
 // Definire las funciones que usare en el programa
-void CambiarColor(int color) { 
-    // Cambia el color del texto en la consola, usare esto para resaltar las letras de busquedad 
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-}
+
 int main() { 
+
+
+
+}
+
+string Minuscula(string palabra) {
+    // Convierte una cadena a minúsculas
+    transform(palabra.begin(), palabra.end(), palabra.begin(), ::tolower);
+    return palabra;
+}
+
+string Marcarr(const string& LasPalabras, const string& Laslineas, int& Eltiempo) {
 
 
 
