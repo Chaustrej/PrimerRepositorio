e15b435ffc351b70263b8504f5a249ee5d93dd40
Chaustrej <131916274+Chaustrej@users.noreply.github.com>
2025-06-19 20:46:47 -0400

    Cree el void de Menu

--- Taller 2/Proyecto2.cpp
+++ Taller 2/Proyecto2.cpp
@@ -14,7 +14,16 @@ struct Producto {
     char categoria[20];
     bool activo;
 };
-
+void MostrarMenu(){
+    cout << "Menu de Productos" << endl;
+    cout << "1. Agregar producto" << endl;
+    cout << "2. Modificar producto" << endl;
+    cout << "3. Eliminar producto" << endl;
+    cout << "4. Listar productos" << endl;
+    cout << "5. Buscar producto" << endl;
+    cout << "6. Salir" << endl;
+    cout << "Seleccione una opcion: ";
+}
 int main () { 
 
     
