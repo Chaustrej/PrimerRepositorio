ccac3e4d0831a081f92b9534389ddaf35e734fa7
Chaustrej <131916274+Chaustrej@users.noreply.github.com>
2025-07-16 23:13:57 -0400

    Push

--- Taller 1/ProyectoJubertPOO.cpp
+++ Taller 1/ProyectoJubertPOO.cpp
@@ -1,167 +1,221 @@
 #include <iostream>
-#include <string>
+#include <fstream>
+#include <cstring>
 using namespace std;
 
-const int maximo_Personas = 200;
+const char* ARCHIVO = "personas.dat";
 
 class Persona {
-   public:
-    string nombre, apellido, cedula;
+private:
+    char nombre[30], apellido[30], cedula[15];
     int edad;
     float coeficiente_int;
-    Persona() {}; 
-    Persona(string nom, string ape, string ced, int ed, float coef) 
-     : nombre(nom), apellido(ape), cedula(ced), edad(ed), coeficiente_int(coef) {}
+    bool activo;
+
+public:
+    Persona() {
+        strcpy(nombre, "");
+        strcpy(apellido, "");
+        edad = 0;
+        strcpy(cedula, "");
+        coeficiente_int = 0;
+        activo = true;
+    }
+
+    void ingresarDatos() {
+        cout << "Nombre: "; cin.ignore(); cin.getline(nombre, 30);
+        cout << "Apellido: "; cin.getline(apellido, 30);
+        cout << "Edad: "; cin >> edad;
+        cout << "Cedula: "; cin.ignore(); cin.getline(cedula, 15);
+        cout << "Coeficiente intelectual: "; cin >> coeficiente_int;
+        activo = true;
+    }
+
     void mostrar() const {
-        cout << "Nombre: " << nombre << " " << apellido
-             << "\nCedula: " << cedula
-             << "\nEdad: " << edad
-             << "\nCoeficiente Intelectual: " << coeficiente_int << endl;
+        cout << "Nombre: " << nombre
+             << ", Apellido: " << apellido
+             << ", Edad: " << edad
+             << ", Cedula: " << cedula
+             << ", Coeficiente: " << coeficiente_int << endl;
+    }
+
+    bool esActiva() const {
+        return activo;
+    }
+
+    const char* getCedula() const {
+        return cedula;
+    }
+
+    int getEdad() const {
+        return edad;
+    }
+
+    void modificar() {
+        cout << "Nuevo nombre: "; cin.ignore(); cin.getline(nombre, 30);
+        cout << "Nuevo apellido: "; cin.getline(apellido, 30);
+        cout << "Nueva edad: "; cin >> edad;
+        cout << "Nuevo coeficiente: "; cin >> coeficiente_int;
+    }
+
+    void desactivar() {
+        activo = false;
     }
 };
 
-class Crud : public Persona {
-    private:
-        Persona personas[maximo_Personas];
-        int cantidad=0;
-    public:
-    void agregarPersonas() {
-      if (cantidad >= maximo_Personas) {
-            cout << "Lista llena. No se pueden agregar más personas.\n";
-            return;
-        }
-        string nom, ape, ced;
-        int ed;
-        float coeficiente_int;
-
-        cout << "Nombre: "; cin >> nom;
-        cout << "Apellido: "; cin >> ape;
-        cout << "Edad: "; cin >> ed;
-        cout << "Cedula: "; cin >> ced;
-        cout << "Coeficiente: "; cin >> coeficiente_int;
-
-        personas[cantidad] = Persona(nom, ape, ced, ed, coeficiente_int);
-        cantidad++;
+class CrudPersonas {
+public:
+    void agregarPersona() {
+        Persona p;
+        p.ingresarDatos();
+        ofstream archivo(ARCHIVO, ios::binary | ios::app);
+        archivo.write((char*)&p, sizeof(Persona));
+        archivo.close();
         cout << "Persona agregada.\n";
     }
+
     void listarPersonas() {
-        if (cantidad == 0) {
-            cout << "No hay personas.\n";
-            return;
-        }
-
-        for (int i = 0; i < cantidad; i++) {
-            personas[i].mostrar();
-        }
-    }
-    void buscarPorEdad() {
-        int edadBuscada;
-        cout << "Edad a buscar: ";
-        cin >> edadBuscada;
-        bool encontrada = false;
-
-        for (int i = 0; i < cantidad; i++) {
-            if (personas[i].edad == edadBuscada) {
-                personas[i].mostrar();
-                encontrada = true;
+        ifstream archivo(ARCHIVO, ios::binary);
+        Persona p;
+        cout << "\n- Lista de personas -\n";
+        while (archivo.read((char*)&p, sizeof(Persona))) {
+            if (p.esActiva()) {
+                p.mostrar();
             }
         }
-
-        if (!encontrada) {
-            cout << "No se encontro a las personas con esas edades.\n";
-        }
+        archivo.close();
     }
+
+    void buscarPorCedula() {
+        char cedula[15];
+        cout << "Ingrese cedula: "; cin.ignore(); cin.getline(cedula, 15);
+        ifstream archivo(ARCHIVO, ios::binary);
+        Persona p;
+        bool encontrado = false;
+
+        while (archivo.read((char*)&p, sizeof(Persona))) {
+            if (p.esActiva() && strcmp(p.getCedula(), cedula) == 0) {
+                p.mostrar();
+                encontrado = true;
+                break;
+            }
+        }
+        archivo.close();
+        if (!encontrado)
+            cout << "Persona no encontrada.\n";
+    }
+
+    void modificarPersona() {
+        char cedula[15];
+        cout << "Ingrese cedula de la persona a modificar: ";
+        cin.ignore();
+        cin.getline(cedula, 15);
+
+        fstream archivo(ARCHIVO, ios::binary | ios::in | ios::out);
+        Persona p;
+        bool encontrado = false;
+
+        while (archivo.read((char*)&p, sizeof(Persona))) {
+            if (p.esActiva() && strcmp(p.getCedula(), cedula) == 0) {
+                cout << "Modificando persona:\n";
+                p.mostrar();
+                p.modificar();
+                archivo.seekp(-sizeof(Persona), ios::cur);
+                archivo.write((char*)&p, sizeof(Persona));
+                cout << "Persona modificada.\n";
+                encontrado = true;
+                break;
+            }
+        }
+        archivo.close();
+        if (!encontrado)
+            cout << "Persona no encontrada.\n";
+    }
+
     void eliminarPersona() {
-        string cedulaBuscada;
-        cout << "Ponga el numero de cedula de la persona que desee eliminar: ";
-        cin >> cedulaBuscada;
+        char cedula[15];
+        cout << "Ingrese cedula de la persona a eliminar: ";
+        cin.ignore();
+        cin.getline(cedula, 15);
 
-        for (int i = 0; i < cantidad; i++) {
-            if (personas[i].cedula == cedulaBuscada) {
-                // Mover los elementos hacia atrás
-                for (int j = i; j < cantidad - 1; j++) {
-                    personas[j] = personas[j + 1];
-                }
-                cantidad--;
+        fstream archivo(ARCHIVO, ios::binary | ios::in | ios::out);
+        Persona p;
+        bool eliminado = false;
+
+        while (archivo.read((char*)&p, sizeof(Persona))) {
+            if (p.esActiva() && strcmp(p.getCedula(), cedula) == 0) {
+                p.desactivar();
+                archivo.seekp(-sizeof(Persona), ios::cur);
+                archivo.write((char*)&p, sizeof(Persona));
                 cout << "Persona eliminada.\n";
-                return;
+                eliminado = true;
+                break;
             }
         }
-        cout << "Persona no encontrada.\n"; }
-    void editarPersona() {
-        string cedulaBuscada;
-        cout << "Cedula a editar: ";
-        cin >> cedulaBuscada;
-
-        for (int i = 0; i < cantidad; i++) {
-            if (personas[i].cedula == cedulaBuscada) {
-                cout << "Nuevo nombre: "; cin >> personas[i].nombre;
-                cout << "Nuevo apellido: "; cin >> personas[i].apellido;
-                cout << "Nueva edad: "; cin >> personas[i].edad;
-                cout << "Nuevo coeficiente: "; cin >> personas[i].coeficiente_int;
-                cout << "Datos actualizados.\n";
-                return;
-            }
-        }
-    cout << "Persona no encontrada.\n";
+        archivo.close();
+        if (!eliminado)
+            cout << "Persona no encontrada.\n";
     }
-     void mostrarPersonasdeMayorEdad() {
-        if (cantidad == 0) {
-            cout << "No hay personas.\n";
-            return;
-        }
 
-        int mayorEdad = personas[0].edad;
-        for (int i = 1; i < cantidad; i++) {
-            if (personas[i].edad > mayorEdad) {
-                mayorEdad = personas[i].edad;
+    void mostrarMayorEdad() {
+        ifstream archivo(ARCHIVO, ios::binary);
+        Persona p;
+        int mayorEdad = -1;
+
+        while (archivo.read((char*)&p, sizeof(Persona))) {
+            if (p.esActiva() && p.getEdad() > mayorEdad) {
+                mayorEdad = p.getEdad();
             }
         }
 
-        cout << "Persona(s) con mayor edad (" << mayorEdad << " anos):\n";
-        for (int i = 0; i < cantidad; i++) {
-            if (personas[i].edad == mayorEdad) {
-                personas[i].mostrar();
+        archivo.clear();
+        archivo.seekg(0);
+        bool encontrado = false;
+        cout << "\n-Personas con mayor edad (" << mayorEdad << " anos):\n";
+        while (archivo.read((char*)&p, sizeof(Persona))) {
+            if (p.esActiva() && p.getEdad() == mayorEdad) {
+                p.mostrar();
+                encontrado = true;
             }
         }
+
+        archivo.close();
+        if (!encontrado)
+            cout << "No hay personas registradas.\n";
     }
-}; 
+};
 
-int main(){
-    Crud crud;
+void mostrarMenu() {
+    cout << "\n--- MENU DE PERSONAS ---\n";
+    cout << "1. Agregar persona\n";
+    cout << "2. Modificar persona\n";
+    cout << "3. Eliminar persona\n";
+    cout << "4. Listar personas\n";
+    cout << "5. Buscar por cedula\n";
+    cout << "6. Mostrar persona(s) de mayor edad\n";
+    cout << "7. Salir\n";
+    cout << "Seleccione una opcion: ";
+}
+
+int main() {
+    CrudPersonas crud;
     int opcion;
 
     do {
-        cout << "\n=== MENU DE PERSONAS ===\n";
-        cout << "1. Agregar persona\n2. Listar personas\n3. Buscar por edad\n";
-        cout << "4. Eliminar persona\n5. Editar persona\n6. Mostrar personas de mayor edad\n7. Salir\nOpcion: ";
+        mostrarMenu();
         cin >> opcion;
 
         switch (opcion) {
-            case 1:
-                crud.agregarPersonas();
-                break;
-            case 2:
-                crud.listarPersonas();
-                break;
-            case 3:
-                crud.buscarPorEdad();
-                break;
-            case 4:
-                crud.eliminarPersona();
-                break;
-            case 5:
-                crud.editarPersona();
-                break;
-            case 6:
-                crud.mostrarPersonasdeMayorEdad();
-                break;
-            case 7:
-                cout << "Saliendo del programa.\n";
-                break;
-            default:
-                cout << "Opcion invalida.\n";
+            case 1: crud.agregarPersona(); break;
+            case 2: crud.modificarPersona(); break;
+            case 3: crud.eliminarPersona(); break;
+            case 4: crud.listarPersonas(); break;
+            case 5: crud.buscarPorCedula(); break;
+            case 6: crud.mostrarMayorEdad(); break;
+            case 7: cout << "Saliendo...\n"; break;
+            default: cout << "Opcion invalida.\n";
         }
+
     } while (opcion != 7);
 
     return 0;
